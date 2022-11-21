ebf974be31832e11688823a6bb75c1312bd4e42e
Amakoye <charlesamakoye@gmail.com>
2022-11-21 10:00:07 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
+import React from "react";
 import "./App.css";
-import Greeting from "./Greeting";
+import Greeting from "./GreetingFunctional";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Greeting name="Dave Choi" />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      enteredName: "",
+      message: "",
+    };
+    this.onChangeName = this.onChangeName.bind(this);
+  }
+  state: { enteredName: string; message: "" };
+
+  onChangeName(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      enteredName: e.target.value,
+      message: `Hello from, ${e.target.value}`,
+    });
+  }
+
+  render(): React.ReactNode {
+    console.log("rendering App");
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input value={this.state.enteredName} onChange={this.onChangeName} />
+          <Greeting message={this.state.message} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/GreetingFunctional.tsx
@@ -0,0 +1,8 @@
+interface GreetingProps {
+  message: string;
+}
+
+export default function Greeting(props: GreetingProps) {
+  console.log("rendering Greeting");
+  return <div>{props.message}</div>;
+}
